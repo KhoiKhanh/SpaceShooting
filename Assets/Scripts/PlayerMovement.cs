42c83458b8f6f1ee3562d3daf8b664c6ef7a9393
KhoiKhanh <Khanhkhoi681@gmail.com>
2026-02-08 22:39:57 +0700

    Quái chết yay

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,9 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         var worldPoint =
-Camera.main.ScreenToWorldPoint(Input.mousePosition);
-worldPoint.z = 0;
-transform.position = worldPoint;
+    Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    worldPoint.z = 0;
+    transform.position = worldPoint;
         Debug.Log(Input.mousePosition);
     }
 }
